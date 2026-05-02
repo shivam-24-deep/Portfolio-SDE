f774f5b9660024a25a0eeafda2af6c88c6d5fdf7
Shivam Mogha <shivammogha73@gmail.com>
2026-05-02 23:41:54 +0530

    Fix: Bypassed TypeScript errors for GSAP trial modules to resolve Netlify build failure

---
+++ src/types/gsap-trial.d.ts
@@ -0,0 +1,13 @@
+declare module 'gsap-trial/ScrollSmoother' {
+  export const ScrollSmoother: any;
+  export default ScrollSmoother;
+}
+
+declare module 'gsap-trial/SplitText' {
+  export const SplitText: any;
+  export default SplitText;
+}
+
+declare module 'gsap-trial/all' {
+  export * from 'gsap-trial';
+}
